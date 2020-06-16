07d317ff096b5b30ffe5c0f971fb60e54deb8878
IgorZhukovMSK <vot_tak78@mail.ru>
2020-06-16 17:28:28 +0300

    Решение ДЗ 1, задача 1

--- src/Lesson1/Task1/ClassGeneric.java
+++ src/Lesson1/Task1/ClassGeneric.java
@@ -2,6 +2,28 @@ package Lesson1.Task1;
 
 public class ClassGeneric<T> extends SwapsElements {
 
+    private T[] obj;
 
+    public ClassGeneric(T... obj) {
+        this.obj = obj;
+    }
+
+    public T[] getObj() {
+        return obj;
+    }
+
+    public void setObj(T[] obj) {
+        this.obj = obj;
+    }
+
+    public void showValue() {
+
+        for (Object i : obj) {
+
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+    }
 
 }
