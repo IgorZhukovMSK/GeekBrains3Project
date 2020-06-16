a9312996b7c852bf7fa3f4136330013cc80ca076
IgorZhukovMSK <vot_tak78@mail.ru>
2020-06-16 17:27:28 +0300

    Решение ДЗ 1, задача 1

--- src/Lesson1/Task1/SwapsElements.java
+++ src/Lesson1/Task1/SwapsElements.java
@@ -2,7 +2,28 @@ package Lesson1.Task1;
 
 public class SwapsElements<T> {
 
+    public void swapsElements(T[] arr, int index1, int index2) {
 
+        T tempElements;
+
+        tempElements = arr[index1];
+
+        arr[index1] = arr[index2];
+        arr[index2] = tempElements;
+    }
+
+    public void swapsElements2(T[] arr) {
+        T tempElements;
+        T firstElement;
+        T lastElement;
+
+        firstElement = arr[0];
+        lastElement = arr[arr.length-1];
+        tempElements = lastElement;
+        arr [0] = tempElements;
+        arr [arr.length - 1] = firstElement;
+
+    }
 
 }
 
